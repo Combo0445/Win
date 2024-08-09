68889728d28470e60af55e4f3fcc4be30e2ec233
Combo0445 <149581752+Combo0445@users.noreply.github.com>
2024-08-09 11:00:09 +0700

    first commit

---
+++ bundle.js
@@ -0,0 +1,6 @@
+function calculate () {
+    var sum = 0;
+    document.getElementById("num1").value;
+    var y =  document.getElementById("num2").value;
+    return sum = parseInt(x) + parseInt(y);
+}
